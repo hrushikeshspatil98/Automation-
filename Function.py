fecc0d33450043ffab345ab68f02914f70641c3b
hrushispatil19 <60781042+hrushispatil19@users.noreply.github.com>
2020-02-10 12:42:05 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,9 @@
+#6 Function without Arguments: 
+def printMsg():                              #Function Definition
+    print("Function without Arguments")
+
+printMsg()                                   #Function Call
+
+""" Output:
+Function without Arguments
+"""
