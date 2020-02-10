e57be35904ad824afb6a9f968a6817b670c13c44
hrushispatil19 <60781042+hrushispatil19@users.noreply.github.com>
2020-02-10 15:43:24 +0530

    Create List2.py

---
+++ List2.py
@@ -0,0 +1,10 @@
+#16 Lists  (Ref. Arrays)
+#List is a collection of elements which is ordered and changeable. Allows duplicate members.
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+""" Output: 
+Yes, 'apple' is in the fruits list
+"""
