cb9c9fbdb78d81bc553a71a8b4f63205cc574183
hrushispatil19 <60781042+hrushispatil19@users.noreply.github.com>
2020-02-10 13:04:24 +0530

    Create CustomException.py

---
+++ CustomException.py
@@ -0,0 +1,17 @@
+#11 Custom Exception
+
+class ErrorInCode(Exception):    
+    def __init__(self, data):    
+        self.data = data    
+    def __str__(self):    
+        return repr(self.data)    
+    
+try:    
+    raise ErrorInCode(2000)    
+except ErrorInCode as ae:    
+    print("Received error:", ae.data)  
+
+"""
+Output:
+Received error: 2000
+"""
