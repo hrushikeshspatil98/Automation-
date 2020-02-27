87f98f5f7ea5b04a93535e9cad3650260177943e
Hrushikesh Patil <60781042+hrushispatil19@users.noreply.github.com>
2020-02-27 10:26:15 +0530

    Create OSModule.py

---
+++ OSModule.py
@@ -0,0 +1,42 @@
+#The python OS module provides functions used for interacting with the operating system and also get related information about it.
+#The python OS module lets us work with the files and directories.
+import os
+
+#This function provides the name of the operating system module that it imports.
+#Currently, it registers 'posix', 'nt', 'os2', 'ce', 'java' and 'riscos'.
+print(os.name)
+print("Current Working Directory is:",os.getcwd()) #Displays Current Working Directory of the file
+
+fd="D:\\HP00661153\\TECHM\\a.txt"
+# Checking path exits or not with os.F_OK   
+path=os.access(fd,os.F_OK)   
+print("Exist path:",path)   
+# Checking read access with os.R_OK   
+path=os.access(fd,os.R_OK)   
+print("access to read the file:",path)       
+# Checking write access with os.W_OK   
+path=os.access(fd,os.W_OK)   
+print("access to write the file:",path)       
+# Checking execute access with os.X_OK   
+path=os.access(fd,os.X_OK)   
+print("access to exicute te path:",path)  
+
+#a file or directory can be renamed by using os.rename() fn,if user has rights to change file
+os.rename(fd,"D:\\HP00661153\\TECHM\\b.txt")
+print("File renamed from a.txt to b.txt") 
+
+#a file or directory is removed by using remove() and rmdir() fn respectively.
+os.remove("D:\\HP00661153\\TECHM\\b.txt")
+os.rmdir("D:\\HP00661153\\TECHM\\a")
+
+"""
+Output:
+
+nt
+Current Working Directory is: C:\Users\HP00661153\.spyder-py3
+Exist path: True
+access to read the file: True
+access to write the file: True
+access to exicute te path: True
+File renamed from a.txt to b.txt
+"""
