036640206571ef8b9de7124c890160943a3e9572
Hrushikesh Patil <60781042+hrushispatil19@users.noreply.github.com>
2020-03-04 10:26:54 +0530

    Create DB.py

---
+++ DB.py
@@ -0,0 +1,25 @@
+import mysql.connector  
+#Create the connection object   
+myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "1234",database = "PythonDB")  
+#creating the cursor object  
+cur = myconn.cursor()  
+sql = "insert into Employee(name, id, salary, dept_id, branch_name) values (%s, %s, %s, %s, %s)"  
+  
+#The row values are provided in the form of tuple   
+val = ("Hrushi", 110, 25000.00, 201, "Pune")  
+  
+try:  
+    #inserting the values into the table  
+    cur.execute(sql,val)  
+  
+    #commit the transaction   
+    myconn.commit()        
+except:  
+    myconn.rollback()  
+  
+print(cur.rowcount,"record inserted")  
+myconn.close()  
+
+'''Output:
+1 record inserted
+'''
